ef0ebede88f9f048827d90312b7a23b1c5105325
irinconc11 <maelrc11@gmail.com>
2026-03-02 17:58:46 -0500

    Primer commit: Estructura base con navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+function Navbar(){
+    return(
+        <nav>
+            <h1>CIAM</h1>
+            <ul>
+                <li>Inicio</li>
+                <li>Política Nacional</li>
+                <li>Paz y Conflicto</li>
+                <li>Quiénes Somos</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
