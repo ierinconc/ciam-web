b45334e4ee0da47bba3fcec28fd28c91d0ca6b5e
irinconc11 <maelrc11@gmail.com>
2026-03-04 22:47:57 -0500

    Mejora del footer con iconos de redes sociales y con los datos reales de CIAM

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import './Footer.css'
+import {FaFacebook, FaInstagram, FaYoutube} from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6'
 
 function Footer(){
     return(
@@ -11,12 +13,13 @@ function Footer(){
                 <p>centrociammedios@gmail.com</p>
                 <p>Calle 38 # 15 - 10, Teusaquillo</p>
                 <p>Bogotá D.C., Colombia</p>
+                <p>Teléfono: +57 302 571 1196</p>
             </div>
             <div className='footer-social'>
-                <p>Twitter / X</p>
-                <p>Facebook</p>
-                <p>Instagram</p>
-                <p>YouTube</p>
+                <a href="https://facebook.com/centrociam" target='_blank'><FaFacebook/> Facebook</a>
+                <a href="https://instagram.com/centro_ciam" target='_blank'><FaInstagram/> Instagram</a>
+                <a href="https://x.com/centro_CIAM" target='_blank'><FaXTwitter/> X/Twitter</a>
+                <a href="https://youtube.com/@centrociam" target='_blank'><FaYoutube/> YouTube</a>
             </div>
             <div className='footer-copy'>
                 <p>© 2026 CIAM. Todos los derechos reservados.</p>
